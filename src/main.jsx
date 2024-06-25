0fb2cbc33145fb5ab61a61de7be0457e272a1984
davidpacco <david.j.pacco@gmail.com>
2024-06-25 18:05:44 -0500

    Add reducer and tests

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import noteReducer from './reducers/noteReducer'
+import { createStore } from 'redux'
+
+const store = createStore(noteReducer)
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    id: 1,
+    content: 'the app state is in redux store',
+    important: true
+  }
+})
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  payload: {
+    id: 2,
+    content: 'state changes are made with actions',
+    important: false
+  }
+})
+
+function App() {
+  return (
+    <div>
+      <ul>
+        {store.getState().map(note => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'Important' : ''}</strong>
+            <button onClick={() => store.dispatch({ type: 'TOGGLE_IMPORTANCE', payload: { id: note.id } })}>Change</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => root.render(<App />)
+
+renderApp()
+store.subscribe(renderApp)
---
+++ src/reducers/noteReducer.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux'
+
+const noteReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'NEW_NOTE':
+      return [...state, action.payload]
+
+    case 'TOGGLE_IMPORTANCE': {
+      const id = action.payload.id
+      const noteToChange = state.find(note => note.id === id)
+      const updatedNote = {
+        ...noteToChange,
+        important: !noteToChange.important
+      }
+
+      return state.map(note => note.id === id ? updatedNote : note)
+    }
+
+    default:
+      return state
+  }
+}
+
+const store = createStore(noteReducer)
+
+export default noteReducer
